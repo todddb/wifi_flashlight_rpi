de07643ffacef7fc9981cb07bac05d635a841660
todddb <todd_berrett@byu.edu>
2025-01-15 08:39:21 -0700

    Update wifi_flashlight.py
    Changed to run on port 80

--- wifi_flashlight.py
+++ wifi_flashlight.py
@@ -117,6 +117,6 @@ led_state = False
 
 if __name__ == "__main__":
     try:
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=80)
     finally:
         GPIO.cleanup()
